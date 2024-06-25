e04671e217df6a543cd923591da21493ec2e7616
Benjamin Brossi <beni.brossi@gmail.com>
2024-06-25 20:17:43 +0200

    fix: server

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,12 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './web-tests',
   reporter: 'html',
+  webServer: {
+    command: 'npm run start',
+    port: 3030,
+  },
   use: {
     trace: 'on-first-retry',
-    launchOptions: {
-      
-    }
   },
   projects: [
     {
